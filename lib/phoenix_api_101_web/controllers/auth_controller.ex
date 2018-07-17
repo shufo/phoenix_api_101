040d872064b0c1497ed7668756fd32a8c1653270
kooo5252 <masaki.affiliate@gmail.com>
2018-07-17 16:31:30 +0900

    Update auth_controller.ex

--- lib/phoenix_api_101_web/controllers/auth_controller.ex
+++ lib/phoenix_api_101_web/controllers/auth_controller.ex
@@ -6,23 +6,22 @@ defmodule PhoenixApi101Web.AuthController do
   alias PhoenixApi101.Accounts.User
   alias JaSerializer.Params
   alias PhoenixApi101.Repo
-  alias ExOauth2Provider.Token
-
-  import ExOauth2Provider.Token
-
   import Ecto.Changeset
   import Plug.Conn
 require Logger
 
-  def create(conn, params) do
-    case Token.grant(params) do
-      {:ok, access_token} ->
-        json(conn, access_token)
+  def create(conn, %{"grant_type" => grant_type, "password" => password, "username" => username}) do
 
-        {:error, error, status} ->
-        conn
-        |> put_status(status)
-        |> json(error)
+    cond do
+      grant_type != "password" -> put_status(conn, :unprocessable_entity)
+
+      username == nil || password == nil -> send_resp(conn, :unauthorized, "")
+
+      true
+        -> conn
+          |> put_status(:created)
+          |> render("index.json-api", data: %{"access_token" => "sfdgfhdfs"})
+        
     end
 
 
